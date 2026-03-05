663569cab459b3e654a864cdccfb882900fcf9f4
sipcoffee <bautistaedward901@gmail.com>
2026-03-05 22:03:13 +0800

    added new models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,12 @@
 import { PrismaClient, PlanType } from "../lib/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log("Seeding database...");
